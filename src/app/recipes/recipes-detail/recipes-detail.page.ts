8c47cdc645011a00bede02635933ca91c0b2e7bd
Rinampanchal <54768928+Rinampanchal@users.noreply.github.com>
2020-08-18 21:54:24 +0530

    Alert controller  and Action Sheet controller demo

--- src/app/recipes/recipes-detail/recipes-detail.page.ts
+++ src/app/recipes/recipes-detail/recipes-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipes-detail',
@@ -12,7 +13,12 @@ export class RecipesDetailPage implements OnInit {
 
   loadedRecipe: Recipe;
 
-  constructor(private activatedRoute: ActivatedRoute, private recipesService:RecipesService,private router:Router) { }
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private recipesService:RecipesService,
+    private router:Router,
+    private altctr: ActionSheetController
+  ) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
@@ -30,9 +36,25 @@ export class RecipesDetailPage implements OnInit {
     });
   }
 
-  onDeleteClick(){
-    this.recipesService.deleteRecipe(this.loadedRecipe.id);
-    this.router.navigate(['recipes']);
-  }
+  async onDeleteClick(){
 
+    const alert = await this.altctr.create({
+      header: 'Are You Sure?',
+      buttons:[
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () =>{
+            this.recipesService.deleteRecipe(this.loadedRecipe.id);
+            this.router.navigate(['recipes']);
+          }
+        }
+      ]
+    });  
+    await alert.present();
+  }
 }
